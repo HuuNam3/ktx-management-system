e936429ae7c535779a09ddcf6f63c7714e6b4698
NguyenHuuNam3 <nhnam4411@gmail.com>
2026-07-17 16:14:15 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Frontend_Management_System_KTX',
+  webDir: 'www'
+};
+
+export default config;
